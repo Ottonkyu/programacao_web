b737014a844a93e1264dc07e9a8a278293897362
Otton Simão Gouveia Cavalcante <135617225+Ottonkyu@users.noreply.github.com>
2025-03-04 14:19:36 -0300

    Add files via upload
    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,65 @@
+// Adicao
+let num1a = 5;
+let num2a = 10;
+let adicao = num1a + num2a;
+console.log(`Resultado da adicao entre ${num1a} e ${num2a} e ${adicao}`);
+
+let num3a = 20;
+let num4a = 30;
+let adicao2 = num3a + num4a;
+console.log(`Resultado da adicao 2 entre ${num3a} e ${num4a} e ${adicao2}`);
+
+// Subtracao
+let num1s = 15;
+let num2s = 10;
+let subtracao = num1s - num2s;
+console.log(`Resultado da subtracao entre ${num1s} e ${num2s} e ${subtracao}`);
+
+let num3s = 20;
+let num4s = 10;
+let subtracao2 = num3s - num4s;
+console.log(`Resultado da subtracao 2 entre ${num3s} e ${num4s} e ${subtracao2}`);
+
+// Multiplicacao
+let num1m = 6;
+let num2m = 7;
+let multiplicacao = num1m * num2m;
+console.log(`Resultado da multiplicacao entre ${num1m} e ${num2m} e ${multiplicacao}`);
+
+let num3m = 9;
+let num4m = 8;
+let multiplicacao2 = num3m * num4m;
+console.log(`Resultado da multiplicacao 2 entre ${num3m} e ${num4m} e ${multiplicacao2}`);
+
+// Divisao
+let num1d = 20;
+let num2d = 4;
+let divisao = num1d / num2d;
+console.log(`Resultado da divisao entre ${num1d} e ${num2d} e ${divisao}`);
+
+let num3d = 50;
+let num4d = 5;
+let divisao2 = num3d / num4d;
+console.log(`Resultado da divisao 2 entre ${num3d} e ${num4d} e ${divisao2}`);
+
+// Modulo 
+let num1mod = 10;
+let num2mod = 3;
+let modulo = num1mod % num2mod;
+console.log(`Resultado do modulo entre ${num1mod} e ${num2mod} e ${modulo}`);
+
+let num3mod = 25;
+let num4mod = 7;
+let modulo2 = num3mod % num4mod;
+console.log(`Resultado do modulo 2 entre ${num3mod} e ${num4mod} e ${modulo2}`);
+
+// Exponenciacao
+let num1exp = 2;
+let num2exp = 3;
+let exponenciacao = num1exp ** num2exp;
+console.log(`Resultado da exponenciacao entre ${num1exp} e ${num2exp} e ${exponenciacao}`);
+
+let num3exp = 5;
+let num4exp = 2;
+let exponenciacao2 = num3exp ** num4exp;
+console.log(`Resultado da exponenciacao 2 entre ${num3exp} e ${num4exp} e ${exponenciacao2}`);
